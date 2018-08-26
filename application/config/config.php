315930628ed3152edb05ffc9a2f84f9402955006
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2018-08-26 02:31:19 -0600

    Cambio de URL en config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ACYE2-Practica1F2-Grupo5/';
+$config['base_url'] = 'http://35.184.59.109/ClimaGT/';
 
 /*
 |--------------------------------------------------------------------------
