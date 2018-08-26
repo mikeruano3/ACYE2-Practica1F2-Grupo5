1f56c987183a6d728b9321f1e159e2968d358fdf
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2018-08-26 02:26:41 -0600

    Agregado de mapa de Google Maps con coordenada estatica

--- application/controllers/Mapa.php
+++ application/controllers/Mapa.php
@@ -5,6 +5,66 @@ class Mapa extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('v_mapa');
+		
+		//creamos la configuración del mapa con un array
+ 		$config = array();
+        
+        //la zona del mapa que queremos mostrar al cargar el mapa
+        //como vemos le podemos pasar la ciudad y el país
+        //en lugar de la latitud y la longitud
+ 		$config['center'] = 'guatemala,guatemala';
+        
+        // el zoom, que lo podemos poner en auto y de esa forma
+        //siempre mostrará todos los markers ajustando el zoom	
+ 		$config['zoom'] = '12'; 
+        
+        //el tipo de mapa, en el pdf podéis ver más opciones
+ 		$config['map_type'] = 'ROADMAP';
+
+        //el ancho del mapa 
+ 		$config['map_width'] = '1520px'; 
+
+        //el alto del mapa	
+ 		$config['map_height'] = '800px'; 
+        
+        //inicializamos la configuración del mapa	
+ 		$this->googlemaps->initialize($config); 
+ 
+ 		//hacemos la consulta al modelo para pedirle 
+ 		//la posición de los markers y el infowindow
+ 		//AQUI VA LA OBTENCIÓN DE LOS DATOS RECOPILADOS
+
+ 		$marker = array();
+            
+        //podemos elegir DROP o BOUNCE
+		$marker ['animation'] = 'DROP';
+        
+        //posición de los markers
+		$marker ['position'] = "14.6157899,-90.5127812";
+        
+        //infowindow de los markers(ventana de información)	
+		$marker ['descripcion'] = "Ubicación de toma de datos de Marvin Calderón";
+        
+        //la id del marker
+		$marker['id'] = 1; 
+			
+		$this->googlemaps->add_marker($marker);
+
+ 		//en $data['datos'tenemos la información de cada marker para
+        //poder utilizarlo en el sidebar en nuestra vista mapa_view
+ 		$data['datos'] = array(array(
+ 			'id' 	=> 1,
+ 			'pos_y' => '-90.5127812',
+ 			'pos_x' => '14.6157899',
+ 			'descripcion' => $marker['descripcion']
+ 		));
+
+        //en data['map'] tenemos ya creado nuestro mapa para llamarlo en la vista
+ 		$data['map'] = $this->googlemaps->create_map();
+
+ 		//cargamos la información a la vista
+ 		$this->load->view('v_mapa', $data);
+
 	}
+
 }
--- application/views/v_mapa.php
+++ application/views/v_mapa.php
@@ -1,10 +1,82 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Mapa de toma de datos | ClimaGT</title>
-	</head>
-	<body>
-		<h1>Hola mundo!</h1>
-	</body>
+	<html lang="es">
+		<head>
+ 			
+ 			<meta charset="utf-8">
+ 			<title>Mapa de recolección de datos | ClimaGT</title>
+ 			<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+ 			<style type="text/css">
+ 				body{
+ 					background: #888888
+ 				}
+ 				#sidebar{
+ 					position: absolute;
+ 					width: 375px;
+					height: 800px;
+					background: #222;
+					color: #fff;
+					margin-left: 1525px;
+					margin-top: -800px;
+					border: 5px solid #fff;
+ 				}
+ 				ul{
+ 					padding: 0;
+ 					text-align: justify; 
+ 				}
+ 				li{
+ 					cursor: pointer;
+ 					border-top: 1px solid #fff;
+ 					background: #c3c3c3; 
+ 					list-style: none;
+ 					color: #111
+ 				}
+ 				li:hover{
+ 					background: #fefefe;
+ 				}
+ 			</style>
+ 			
+ 			<script type="text/javascript">
+ 				
+ 				function datos_marker(lat, lng, marker) {
+     				
+     				var mi_marker = new google.maps.LatLng(lat, lng);
+     				map.panTo(mi_marker);
+     				google.maps.event.trigger(marker, 'click');
+    			}
+
+ 			</script>
+ 			
+ 			<?php echo $map['js']; ?>
+ 			
+		</head>
+		
+		<body>
+			
+			<div class="alert alert-success">
+			  	<strong><h1 style="text-align:center;"> UBICACIÓN DE RECOPILACIÓN DE INFORMACION | CLIMAGT </h1></strong>.
+			</div>
+
+			<div style="text-align:center;">
+				<?php echo $map['html']; ?>
+			</div>
+
+			<div id="sidebar">
+ 				
+ 				<ul>
+ 					
+ 					<?php foreach($datos as $marker_sidebar){ ?>
+ 						
+ 						<li onclick="datos_marker(<?php echo $marker_sidebar['pos_y']; ?>, <?php echo $marker_sidebar['pos_x']; ?>, marker_<?php echo $marker_sidebar['id']; ?>)">
+ 							<?php echo $marker_sidebar['id']; ?> &nbsp;&nbsp; <?php echo $marker_sidebar['descripcion']; ?>
+ 						</li>
+
+ 					<?php } ?>
+
+ 				</ul>
+
+			</div>
+
+		</body>
+
 </html>
