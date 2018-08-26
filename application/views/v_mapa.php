9f60192384bde2838b7df3a9cb123a93e9e8ee50
Marvin Calderón <2327239740101@ingenieria.usac.edu.gt>
2018-08-26 01:32:50 -0600

    Agregado de controlador y vista para colocacion de mapa

---
+++ application/views/v_mapa.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mapa de toma de datos | ClimaGT</title>
+	</head>
+	<body>
+		<h1>Hola mundo!</h1>
+	</body>
+</html>
